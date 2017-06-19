6321b39acd96110a0e132e520c403f8be7bffb49
WashingtonSouza <thewashington.souza@gmail.com>
2017-06-19 15:36:50 -0300

    Sessões e autenticação de usuário.

---
+++ CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        public object UsuarioDao { get; private set; }
+
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Autentica(Usuario usuario)
+        {
+            UsuariosDAO dao = new UsuariosDAO();
+            
+            if (dao.Busca(usuario.Nome, usuario.Senha) != null)
+            {
+                Session["UsuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
